849bad5e3e950d4afd28169fabb2eb59bc88b5b7
ChandraPrakashSah <111756241+ChandraPrakashSah@users.noreply.github.com>
2022-09-20 18:44:52 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,105 +1,128 @@
-let tiecount = 0;
-var tie = document.getElementById("tie");
-var count = 0;
 let player1 = "X";
 let player2 = "O";
 let copy = 0;
-let idco = 0;
-let arr;
 let getid;
-let cbtn=document.getElementById('cbtn');
-cbtn.disabled=true;
-// let x=document.getElementById('win');
+let cbtn = document.getElementById("cbtn");
+cbtn.disabled = true;
 let container2 = document.getElementById("container2");
 var size;
-// console.log(arr);
-
-
-
 
 let obj = [
   {
-    id:0,
+    id: 0,
     count: 0,
     arr: [],
-    size: 0,
-    copy: 0,
-    result: '',
-    tiecount:0,
-    Digarr1:[],
-    Digarr2:[],
-    Digarr3:[],
-    Digarr4:[],
-    Digx:0,
-    Digo :0,
+    Digx: 0,
+    Digo: 0,
     Dig2x: 0,
     Dig2o: 0,
+    player1: "X",
+    player2: "O",
   },
 ];
 
+//  local storage
+
+let data = JSON.parse(localStorage.getItem("myobj"));
+if (data) {
+  data.forEach((element) => {
+    obj.push(element);
+    game(element);
+    
+    
+  });
+
+}
+
+
+///   start the game
 function onStart() {
-  cbtn.disabled=false;
+  size = document.getElementById("input").value;
+  for (let i = 0; i < size; i++) {
+    obj[0].arr[i] = [];
+  }
+  game(obj[0]);
+}
+handleDelete = (ev, id, x) => {
+  // obj = obj.filter((m) => m.id !== id);
+
+  // obj.splice(id, 1);
+  const a = document.getElementById(`_inner${id}`);
+  a.remove();
+  ev.target.remove();
+  // console.log(obj);
+  x.remove();
+
+  localStorage.setItem("myobj", JSON.stringify(obj));
+};
+
+function game(curr) {
+  // console.log(curr);
+  cbtn.disabled = false;
   document.getElementById("btn").disabled = true;
 
   size = document.getElementById("input").value;
   let container = document.createElement("div");
-  container.setAttribute("id", `_inner${copy}`);
+  container.setAttribute("id", `_inner${curr.id}`);
   container2.appendChild(container);
+  // let x = document.createElement("p");
+  // x.setAttribute("id", `in${curr.id}`);
+  // container2.append(x);
+  const delBtn = document.createElement("button");
+  delBtn.addEventListener("click", (ev) => {
+    handleDelete(ev, curr.id, x);
+  });
+  delBtn.innerText = "Delete";
+  container2.appendChild(delBtn);
+
+  //
   let x = document.createElement("p");
-  x.setAttribute("id", `in${copy}`);
-  // let delbtn=document.createElement('button');
-  // container2.appendChild(delbtn);
-  // delbtn.classList.add("dlbtn");
-  //  delbtn.innerHTML="delete";
-
+  x.setAttribute("id", `in${curr.id}`);
   container2.append(x);
+  //
 
-  let i = document.getElementById(`_inner${copy}`);
+  let i = document.getElementById(`_inner${curr.id}`);
   i.style.display = "flex";
   i.style.justifyContent = "center";
-  i.style.marginTop = "50px";
+  i.style.marginTop = "10px";
 
-  // delbtn.onclick=function(){
-  //   let i=document.getElementById(`_inner${copy}`);
-  //   i.style.display="none";
-
+  /***create multidiamensional arr */
+  // for (let i = 0; i < size; i++) {
+  //   curr.arr[i] = [];
   // }
 
-  /***creation of arr */
-  obj[copy].arr = [];
-  for (let i = 0; i < size; i++) {
-    obj[copy].arr[i] = [];
-  }
   /******** */
   for (let i = 0; i < size; i++) {
     var a = document.createElement("div");
-    // var b = document.getElementById("inner");
     container.appendChild(a);
     /*************/
     for (let j = 0; j < size; j++) {
       var c = document.createElement("div");
       c.classList.add("box2");
+
+      // print array after refresh
+      if (curr.arr[j][i]){ 
+      c.innerHTML = curr.arr[j][i];
+    
+    }
+
+
       a.appendChild(c);
       //********* */
-      c.setAttribute("id", `div_${copy + "_" + j + "" + "_" + i}`);
+      c.setAttribute("id", `div_${curr.id + "_" + j + "" + "_" + i}`);
 
       c.onclick = function () {
-        // console.log(this.id);
         getid = document.getElementById(this.id);
-
-        //***** */
-
         if (getid.innerHTML == "" && x.innerHTML == "") {
-          if (obj[copy].count % 2 == 0) {
+          if (obj[curr.id].count % 2 == 0) {
             getid.innerHTML = player1;
-            obj[copy].count++;
-            stokePlayer(x, player1, j, i);
+            obj[curr.id].count++;
+            stokePlayer(curr, x, obj[curr.id].player1, j, i, obj[curr.id].count);
           } else {
             getid.innerHTML = player2;
-            obj[copy].count++;
-            stokePlayer(x, player2, j, i);
+            obj[curr.id].count++;
+            stokePlayer(curr, x, obj[curr.id].player2, j, i, obj[curr.id].count);
           }
-          //
           if (x.innerHTML != "") {
             document.getElementById("cbtn").disabled = true;
           }
@@ -110,81 +133,38 @@ function onStart() {
 }
 
 //adding player1 and  player2 in multidimensional array
-function stokePlayer(x, player,j, i) {
-  obj[copy].arr[j][i] = player;
-  console.log(obj)
-  // console.log(obj[copy].arr);
-  // let resultWinner = document.getElementById(`in${copy}`);
-  // console.log(resultWinner);
-   // Digonal 1
+function stokePlayer(curr, x, player, j, i, count) {
+  obj[curr.id].arr[j][i] = player;
 
-
-  //  m=j n=i
-
-   if (j == i && obj[copy].arr[j][i] == player1) {
-    obj[copy].Digarr1.push(player1);
-      
-    if (obj[copy].Digarr1.length == size) {
-      x.innerHTML=`${player1} has won`;
-      // obj[copy].result = `${player1} has won`;
-      // resultWinner.innerHTML = obj[copy].result;
-    }
-  } else if (j == i && obj[copy].arr[j][i] == player2) {
-       obj[copy].Digarr2.push(player2);
-       
-    if (obj[copy].Digarr2.length== size) {
-         x.innerHTML=`${player2} has won`;
-      // obj[copy].result = `${player2} has won`;
-      // resultWinner.innerHTML = obj[copy].result;
+  // localStorage.setItem("myobj", JSON.stringify(obj));
+  console.log(obj);
+  // Digonal 1
+  if (j == i && obj[curr.id].arr[j][i] == player1) {
+    obj[curr.id].Digx++;
+    if (obj[curr.id].Digx == size) {
+      x.innerHTML = `${player1} has won`;
     }
   }
-
+  if (j == i && obj[curr.id].arr[j][i] == player2) {
+    obj[curr.id].Digo++;
+    if (obj[curr.id].Digo == size) {
+      x.innerHTML = `${player2} has won`;
+    }
+  }
   //  Digonal 2
 
-  if (j + i == size - 1 && obj[copy].arr[j][i] == player1) {
-      obj[copy].Digarr3.push(player1);
-     
-    if (obj[copy].Digarr3.length == size) {
-       x.innerHTML=`${player1} has won`;
-      // obj[copy].result = `${player1} has won`;
-      // resultWinner.innerHTML = obj[copy].result;
-    }
-  } else if (j + i == size - 1 && obj[copy].arr[j][i] == player2) {
-        obj[copy].Digarr4.push(player2);
-        
-    if (obj[copy].Digarr4.length== size) {
-          x.innerHTML=`${player2} has won`;
-      // obj[copy].result = `${player2} has won`;
-      // resultWinner.innerHTML = obj[copy].result;
+  if (j + i == size - 1 && obj[curr.id].arr[j][i] == player1) {
+    obj[curr.id].Dig2x++;
+    if (obj[curr.id].Dig2x == size) {
+      x.innerHTML = `${player1} has won`;
     }
   }
-
-
-  // let Digarr1 = [];
-  // let Digarr2 = [];
-  // let Digarr3 = [];
-  // let Digarr4 = [];
-  // let Digx = 0;
-  // let Digo = 0;
-  // let Dig2x = 0;
-  // let Dig2o = 0;
-
-
-
-// condition for tie
-
- if( obj[copy].Digarr1.length!=size || obj[copy].Digarr2.length!=size ||  obj[copy].Digarr3.length!=size  || obj[copy].Digarr4.length!=size){
-              if(j==size-1 && i==size-1){
-                  obj[copy].tiecount++;
-                  // console.log(tiecount);
-                  if(obj[copy].tiecount==size*size){
-                    console.log("tie");
-                    x.innerHTML="Game Tie";
-                    // if(x.innerHTML==''){
-                    // }
-                  }
-              }
-       }
+  if (j + i == size - 1 && obj[curr.id].arr[j][i] == player2) {
+    obj[curr.id].Dig2o++;
+    if (obj[curr.id].Dig2o == size) {
+      x.innerHTML = `${player2} has won`;
+    }
+  }
 
   for (let m = 0; m < size; m++) {
     let Rowx = 0;
@@ -193,157 +173,85 @@ function stokePlayer(x, player,j, i) {
     let Colo = 0;
     for (let n = 0; n < size; n++) {
       /*******Row */
-      // console.log(arr[m][n]);
-      if (obj[copy].arr[m][n] == player1) {
-        // console.log(Rowx);
-          Rowx++;
-           
-        if (  Rowx == size) {
+      if (obj[curr.id].arr[m][n] == player1) {
+        Rowx++;
+        if (Rowx == size) {
           x.innerHTML = `${player1} has won`;
-             Rowx=0;
-          // obj[copy].result = `${player1} has won`;
-          // resultWinner.innerHTML = obj[copy].result;
         }
-      } if (obj[copy].arr[m][n] == player2) {
-             Rowo++;
-          if (Rowo == size) {
-            console.log(Rowo);
-            x.innerHTML = `${player2} has won`;
-            Rowo=0;
-            // obj[copy].result = `${player2} has won`;
-            // resultWinner.innerHTML = obj[copy].result;
-          }
+      }
+      if (obj[curr.id].arr[m][n] == player2) {
+        Rowo++;
+        if (Rowo == size) {
+          x.innerHTML = `${player2} has won`;
+        }
       }
       /*******Column */
 
-      if (obj[copy].arr[n][m] == player1) {
-             Colx++;
+      if (obj[curr.id].arr[n][m] == player1) {
+        Colx++;
         if (Colx == size) {
           x.innerHTML = `${player1} has won`;
-            Colx=0;
-          // obj[copy].result = `${player1} has won`;
-          // resultWinner.innerHTML = obj[copy].result;
         }
-      } if (obj[copy].arr[n][m] == player2) {
-             Colo++;
+      } else if (obj[curr.id].arr[n][m] == player2) {
+        Colo++;
         if (Colo == size) {
           x.innerHTML = `${player2} has won`;
-          Colo=0;
-          // obj[copy].result = `${player2} has won`;
-          // resultWinner.innerHTML = obj[copy].result;
         }
       }
-      // // Digonal 1
-
-      // if (m == n && obj[copy].arr[m][n] == player1) {
-      //   obj[copy].Digarr1.push(player1);
-          
-      //   if (obj[copy].Digarr1.length == size) {
-      //     x.innerHTML=`${player1} has won`;
-      //     // obj[copy].result = `${player1} has won`;
-      //     // resultWinner.innerHTML = obj[copy].result;
-      //   }
-      // } else if (m == n && obj[copy].arr[m][n] == player2) {
-      //      obj[copy].Digarr2.push(player2);
-           
-      //   if (obj[copy].Digarr2.length== size) {
-      //        x.innerHTML=`${player2} has won`;
-      //     // obj[copy].result = `${player2} has won`;
-      //     // resultWinner.innerHTML = obj[copy].result;
-      //   }
-      // }
-
-      // //  Digonal 2
-
-      // if (m + n == size - 1 && obj[copy].arr[m][n] == player1) {
-      //     obj[copy].Digarr3.push(player1);
-         
-      //   if (obj[copy].Digarr3.length == size) {
-      //      x.innerHTML=`${player1} has won`;
-      //     // obj[copy].result = `${player1} has won`;
-      //     // resultWinner.innerHTML = obj[copy].result;
-      //   }
-      // } else if (m + n == size - 1 && obj[copy].arr[m][n] == player2) {
-      //       obj[copy].Digarr4.push(player2);
-            
-      //   if (obj[copy].Digarr4.length== size) {
-      //         x.innerHTML=`${player2} has won`;
-      //     // obj[copy].result = `${player2} has won`;
-      //     // resultWinner.innerHTML = obj[copy].result;
-      //   }
-      // }
-      //  if((Rowo==size || Rowx==size)  ||  (Colx==size  || Colo==size)  || (Digarr1.length==size) ||      (Digarr2.length==size)  ||  (Digarr3.length==size)  || (Digarr4.length==size)){
-      //        x.innerHTML="Game over";
-
-      //  }
-      //conditon for Tie the game
-       if( Rowo!=size ||Rowx!=size  || Colx!=size  || Colo!=size){
-              if(m==size-1 && n==size-1){
-                  obj[copy].tiecount++;
-                  // console.log(tiecount);
-                  if(obj[copy].tiecount==size*size){
-                    console.log("tie");
-                    x.innerHTML="Game Tie";
-                    // if(x.innerHTML==''){
-                    // }
-                  }
-              }
-       }
-
-    } 
+      if (count === size * size && x.innerHTML == "") {
+        x.innerHTML = "Game Tie";
+      }
+    }
   }
+  localStorage.setItem("myobj", JSON.stringify(obj));
 }
-
 // copy the game
-
 function copyGame() {
   copy++;
-
   let copyobj = {
     id: copy,
     count: 0,
     arr: [],
-    size: 0,
-    result: "",
-    tiecount:0,
-    Digarr1:[],
-    Digarr2:[],
-    Digarr3:[],
-    Digarr4:[],
-    Digx:0,
-    Digo :0,
+    Digx: 0,
+    Digo: 0,
     Dig2x: 0,
     Dig2o: 0,
+    player1: "X",
+    player2: "O",
   };
 
-  obj.push(copyobj);
- 
-  obj[copy].count = obj[0].count;
-  obj[copy].size = obj[0].size;
-  obj[copy].Digarr1 = obj[0].Digarr1;
-  obj[copy].Digarr2 = obj[0].Digarr2;
-  obj[copy].Digarr3 = obj[0].Digarr3;
-  obj[copy].Digarr4 = obj[0].Digarr4;
-  obj[copy].tiecount = obj[0].tiecount;
-  // console.log(copyobj)
-   for(let i=0;i<obj[0].arr.length;i++){
-    obj[copy].arr[i]=[];
-   }
- 
-  onStart();
+    obj.push(copyobj);
 
-  for (let i = 0; i < obj[copy].arr.length; i++) {
-    for (let j = 0; j < obj[copy].arr.length; j++) {
+  //  previous state copy
+  obj[copy].count = obj[copy - 1].count;
+  obj[copy].Digx = obj[copy - 1].Digx;
+  obj[copy].Digo = obj[copy - 1].Digo;
+  obj[copy].Dig2x = obj[copy - 1].Dig2x;
+  obj[copy].Dig2o = obj[copy - 1].Dig2o;
+
+  // size = document.getElementById("input").value;
+  for (let i = 0; i < size; i++) {
+    obj[copy].arr[i] = [];
+  }
+
+  game(obj[copy]);
+
+  // get dat from localstorage
+  let data1 = JSON.parse(localStorage.getItem("myobj"));
+
+  for (let i = 0; i < obj[copy - 1].arr.length; i++) {
+    for (let j = 0; j < obj[copy - 1].arr.length; j++) {
       if (obj[copy - 1].arr[j][i] != undefined) {
-        const diff = document.getElementById(
+        let idc = document.getElementById(
           `div_${copy + "_" + j + "" + "_" + i}`
         );
-
         obj[copy].arr[j][i] = obj[copy - 1].arr[j][i];
-        diff.innerHTML = obj[copy].arr[j][i];
+        idc.innerHTML = obj[copy - 1].arr[j][i];
       }
     }
   }
-    
+  localStorage.setItem("myobj", JSON.stringify(obj));
+
+
 }
 
